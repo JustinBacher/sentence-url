0030dd9108cb7b69f3bfbb0850e0e016542e881c
92JBach <92jbach@gmail.com>
2019-03-18 15:33:25 -0400

    Fixed errors

--- sentence-url.py
+++ sentence-url.py
@@ -2,41 +2,43 @@ from random import choice, sample
 
 vowels = {'a', 'e', 'i', 'o', 'u'}
 
-with open("adjectives.txt") as adjs_file:
+with open("words/adjectives.txt") as adjs_file:
   adjectives = adjs_file.read().split()
-with open("words.txt") as words_file:
+with open("words/nouns.txt") as words_file:
   nouns = words_file.read().split()
 
 class SentenceURL:
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, word_count = 3, capitalize = True, seperator = ''):
+        self.self.word_count = word_count
+        self.capitalize = capitalize
+        self.seperator = seperator
 
-    def generate(word_count = 3, capitalize = True, seperator = ''):
+    def generate():
       """Generates readable URLs like Twitch's clips"""
 
-      if word_count < 2:
+      if self.word_count < 2:
         raise ValueError('Minimum value expected: 2')
-      elif word_count > 10:
+      elif self.word_count > 10:
         raise ValueError('Maximum value expected: 10')
 
       noun = choice(nouns)
       word_list = []
 
-      if word_count > 3:
+      if self.word_count > 3:
         if noun in vowels:
           word_list = ['an']
         else:
           word_list = [choice(('a', 'the'))]
 
-      word_list.extend(sample(adjectives, k = word_count-1))
+      word_list.extend(sample(adjectives, k = self.word_count-1))
 
-      if word_count > 4:
+      if self.word_count > 4:
         word_list.insert(2, 'and')
 
       word_list.append(noun)
 
-      if capitalize:
+      if self.capitalize:
         word_list = map(str.title, word_list)
 
-      return seperator.join(word_list)
+      return self.seperator.join(word_list)
